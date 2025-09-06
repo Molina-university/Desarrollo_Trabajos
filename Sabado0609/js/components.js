2c3496a4ba78f3973b4f7cddc0b81d752233ab30
Moli <jamolinag@ufpso.edu.co>
2025-09-06 07:46:57 -0600

    Pau Pau pau pau pau pau pauu

--- Sabado0609/js/components.js
+++ Sabado0609/js/components.js
@@ -3,4 +3,6 @@ async function cargarComponente(id, url) {
     const html = await resp.text();
     document.getElementById(id).innerHTML = html;
   }
-  cargarComponente("header", "components/header.html", "components/footer.html", "components/content.html");
+  cargarComponente("header", "components/header.html");
+  cargarComponente("components", "components/footer.html");
+  cargarComponente("content", "components/content.html");
