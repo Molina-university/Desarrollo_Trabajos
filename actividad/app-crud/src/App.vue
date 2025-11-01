be3e1f687ad35a6518f7b2f9d14cc806463aaa00
Moli <jamolinag@ufpso.edu.co>
2025-11-01 17:49:34 -0600

    Un chingo de cosas

---
+++ actividad/app-crud/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+nav {
+  padding: 1rem;
+  background: #f0f0f0;
+}
+nav a {
+  margin-right: 1rem;
+  text-decoration: none;
+  color: #333;
+}
+nav a.router-link-active {
+  font-weight: bold;
+}
+</style>
---
+++ actividad/app-crud/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from './router' // tu archivo de rutas
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
